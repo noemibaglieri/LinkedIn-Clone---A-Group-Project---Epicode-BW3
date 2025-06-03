ee510e533030785add7ca071ecc3369d5510e1ea
Cosimo0C <cosimocrupi01@gmail.com>
2025-06-03 14:25:19 +0200

    navbar-completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 import UserProfileCard from "./components/UserProfileCard";
 
 function App() {
   return (
     <>
+      <NavBar />
       <UserProfileCard />
     </>
   );
