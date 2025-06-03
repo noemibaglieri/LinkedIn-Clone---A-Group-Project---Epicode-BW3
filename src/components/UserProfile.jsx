c8c1bcde3bebb9c22a770d3490385e41a2e9f677
Noemi Baglieri <noemibaglieri@gmail.com>
2025-06-03 17:18:50 +0200

    activity component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,18 +2,19 @@ import { Col, Row } from "react-bootstrap";
 import ColumnProfileRight from "./ColumnProfileRight";
 import UserProfileCard from "./UserProfileCard";
 import Experiences from "./Experiences";
+import Activity from "./Activity";
 
 const UserProfile = () => {
   return (
     <Row>
       <Col md={8}>
         <UserProfileCard />
+        <Activity />
+        <Experiences />
       </Col>
+
       <Col md={4}>
         <ColumnProfileRight />
-      </Col>
-      <Col md={8}>
-        <Experiences />
       </Col>
     </Row>
   );
